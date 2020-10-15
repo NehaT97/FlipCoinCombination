8072c53493fdb20ef1058e1cdb280e20e3e93b74
unknown <nehathakur764@gmail.com>
2020-10-15 16:17:07 +0530

    doublet combination Percentage

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,74 +2,14 @@
 
 #WELCOME TO FLIP COIN SIMULATION#
 
-#function singlet
-function singlet()
-{
-	HEAD=0;
-	headcount=0;
-	tailcount=0;
-	temp=100
-	
-	for (( count=0;count<$times;count++ ))
-        do
-                flipcoin=$((RANDOM%2))
-                if [[ $flipcoin -eq $HEAD ]]
-                then
-                        echo "head"
-                        singletFlip[HEAD]=$((++headcount))
-                else
-
-                        echo "tail"
-                        singletFlip[Tail]=$((++tailcount))
-
-                fi
-        done
-
-	declare -A singletFlip
-	echo ""
-	echo "Enter the number of time you want to flip coin:"
-	read times
-	#temp=100;
-	echo ""
-	for (( count=0;count<$times;count++ ))
-	do
-		flipcoin=$((RANDOM%2))
-		if [[ $flipcoin -eq $HEAD ]]	
-		then
-			echo "head"
-			singletFlip[HEAD]=$((++headcount))
-		else
-		
-			echo "tail"
-			singletFlip[Tail]=$((++tailcount))
-
-		fi
-	done
-
-	let singletHeadPercentage=($headcount*$temp)/$times
-	echo""
-	echo "Single Head Count :" $headcount
-	echo ""
-	echo "Single Head Percentage :" $singletHeadPercentage
-
-
-	let singletTailPercentage=($tailcount*$temp)/$times
-        echo""
-        echo "Single tail Count :" $tailcount
-        echo ""
-        echo "Single tail Percentage :" $singletTailPercentage}
-}
-
+#function  Doublet
 function Doublet()
 {
 	HEAD=0;
 	noofcoin=2;
 
-        headcount=0;
-        tailcount=0;
         temp=100
 	
-
 	declare -A doubletFlip
         echo ""
         echo "Enter the number of time you want to flip coin:"
@@ -108,7 +48,6 @@ function Doublet()
 main()
 {
 
-#singlet
 Doublet
 
 }
