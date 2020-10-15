e2acef45f741fd7aa1b30ba383ad03daa992770b
unknown <nehathakur764@gmail.com>
2020-10-15 12:31:44 +0530

    To check Head and Tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,1 +1,15 @@
+#!/bin/bash -x
+
 #WELCOME TO FLIP COIN SIMULATION#
+
+HEAD=0;
+TAIL=1;
+
+flipcoin=$((RANDOM%2))
+#echo $flipcoin
+if [[ $flipcoin -eq $HEAD ]]
+then
+	echo "head"
+else
+	echo "tail"
+fi
