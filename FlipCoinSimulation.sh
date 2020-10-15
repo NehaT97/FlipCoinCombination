4f12c7b09bf69280faa1f27fcd61d3884dd87d8f
unknown <nehathakur764@gmail.com>
2020-10-15 14:11:39 +0530

    Function for singlet Combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,14 +2,52 @@
 
 #WELCOME TO FLIP COIN SIMULATION#
 
-HEAD=0;
-TAIL=1;
+function singlet()
+{
+	HEAD=0;
+	headcount=0;
+	tailcount=0;
+	temp=100
 
-flipcoin=$((RANDOM%2))
-#echo $flipcoin
-if [[ $flipcoin -eq $HEAD ]]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+	declare -A singletFlip
+	echo ""
+	echo "Enter the number of time you want to flip coin:"
+	read times
+	#temp=100;
+	echo ""
+	for (( count=0;count<$times;count++ ))
+	do
+		flipcoin=$((RANDOM%2))
+		if [[ $flipcoin -eq $HEAD ]]	
+		then
+			echo "head"
+			singletFlip[HEAD]=$((++headcount))
+		else
+		
+			echo "tail"
+			singletFlip[Tail]=$((++tailcount))
+
+		fi
+	done
+
+	let singletHeadPercentage=($headcount*$temp)/$times
+	echo""
+	echo "Single Head Count :" $headcount
+	echo ""
+	echo "Single Head Percentage :" $singletHeadPercentage
+
+
+	let singletTailPercentage=($tailcount*$temp)/$times
+        echo""
+        echo "Single tail Count :" $tailcount
+        echo ""
+        echo "Single tail Percentage :" $singletTailPercentage
+
+}
+
+
+main()
+{
+singlet
+}
+main
