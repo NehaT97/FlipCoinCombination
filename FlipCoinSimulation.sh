03fa0e7fdc5278f76553c8c7ad2ccd3c1e79af8e
unknown <nehathakur764@gmail.com>
2020-10-15 16:00:37 +0530

    function for doublet combination

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,12 +2,28 @@
 
 #WELCOME TO FLIP COIN SIMULATION#
 
+#function singlet
 function singlet()
 {
 	HEAD=0;
 	headcount=0;
 	tailcount=0;
 	temp=100
+	
+	for (( count=0;count<$times;count++ ))
+        do
+                flipcoin=$((RANDOM%2))
+                if [[ $flipcoin -eq $HEAD ]]
+                then
+                        echo "head"
+                        singletFlip[HEAD]=$((++headcount))
+                else
+
+                        echo "tail"
+                        singletFlip[Tail]=$((++tailcount))
+
+                fi
+        done
 
 	declare -A singletFlip
 	echo ""
@@ -41,13 +57,61 @@ function singlet()
         echo""
         echo "Single tail Count :" $tailcount
         echo ""
-        echo "Single tail Percentage :" $singletTailPercentage
-
+        echo "Single tail Percentage :" $singletTailPercentage}
 }
 
+function Doublet()
+{
+	HEAD=0;
+	noofcoin=2;
+
+        headcount=0;
+        tailcount=0;
+        temp=100
+	
+
+	declare -A doubletFlip
+        echo ""
+        echo "Enter the number of time you want to flip coin:"
+        read times
+	
+	for (( count=0;count<$times;count++ ))
+        do
+		for (( count1=0;count1<$noofcoin;count1++ ))
+                do
+			flipcoin=$((RANDOM%2))
+            
+		    	if [[ $flipcoin -eq $HEAD ]]
+         		then
+            			coinSide+=H
+         		else
+            			coinSide+=T
+         		fi
+                done
+                	((doubletFlip[$coinSide]++))
+                	coinSide=""
+        done
+
+	
+	function TotalDoublePercentage()
+	{
+
+	   for index in ${!doubletFlip[@]}
+       	   do
+     		let doubletFlip[$index]=(${doubletFlip[$index]} * 100 / $numberOfCoinFlip)
+		
+   	  done 
+		echo "doubletflip:" $doubletFlip[$index]
+	}
+}
 
 main()
 {
-singlet
+
+#singlet
+Doublet
+
 }
 main
+
+
